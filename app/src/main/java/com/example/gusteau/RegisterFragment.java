c1940887c8c51bda6932de2d16ff8694c4848c1a
Osama Khaled <osamakhaled31415@gmail.com>
2026-01-31 17:30:05 +0200

    Register Screen UI

--- app/src/main/java/com/example/gusteau/RegisterFragment.java
+++ app/src/main/java/com/example/gusteau/RegisterFragment.java
@@ -2,63 +2,70 @@ package com.example.gusteau;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class RegisterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ImageButton btnBack;
+    private TextInputEditText etName;
+    private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private TextInputEditText etConfirmPassword;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextInputLayout tilName;
+    private TextInputLayout tilEmail;
+    private TextInputLayout tilPassword;
+    private TextInputLayout tilConfirmPassword;
 
-    public RegisterFragment() {
-        // Required empty public constructor
-    }
+    private MaterialButton btnSignup;
+    private MaterialButton btnGoogleSignup;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView tvLogin;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize all views using view.findViewById
+        btnBack = view.findViewById(R.id.btn_back);
+
+        etName = view.findViewById(R.id.et_name);
+        etEmail = view.findViewById(R.id.et_email);
+        etPassword = view.findViewById(R.id.et_password);
+        etConfirmPassword = view.findViewById(R.id.et_confirm_password);
+
+        tilName = view.findViewById(R.id.til_name);
+        tilEmail = view.findViewById(R.id.til_email);
+        tilPassword = view.findViewById(R.id.til_password);
+        tilConfirmPassword = view.findViewById(R.id.til_confirm_password);
+
+
+        btnSignup = view.findViewById(R.id.btn_signup);
+        btnGoogleSignup = view.findViewById(R.id.btn_google_signup);
+
+        tvLogin = view.findViewById(R.id.tv_login);
+
+        progressBar = view.findViewById(R.id.progress_bar);
     }
 }
