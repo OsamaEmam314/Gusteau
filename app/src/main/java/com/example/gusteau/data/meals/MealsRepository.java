8ba5c940140265ad014a7317cfc702f5d215fd5e
Osama Khaled <osamakhaled31415@gmail.com>
2026-02-07 18:44:32 +0200

    primary key nonNull

--- app/src/main/java/com/example/gusteau/data/meals/MealsRepository.java
+++ app/src/main/java/com/example/gusteau/data/meals/MealsRepository.java
@@ -1,4 +1,19 @@
 package com.example.gusteau.data.meals;
 
+import android.content.Context;
+
+import com.example.gusteau.data.meals.datasource.local.MealLocalDataSource;
+import com.example.gusteau.data.meals.datasource.local.MealSharedPrefrenceLocalDataSource;
+import com.example.gusteau.data.meals.datasource.remote.RemoteMealDataSource;
+
 public class MealsRepository {
+    private final RemoteMealDataSource remoteDataSource;
+    private final MealLocalDataSource localDataSource;
+    private final MealSharedPrefrenceLocalDataSource sharedPrefrenceLocalDataSource;
+    public MealsRepository(Context context) {
+        this.remoteDataSource = new RemoteMealDataSource();
+        this.localDataSource = new MealLocalDataSource(context);
+        this.sharedPrefrenceLocalDataSource = new MealSharedPrefrenceLocalDataSource(context);
+    }
+
 }
--- app/src/main/java/com/example/gusteau/data/model/Meal.java
+++ app/src/main/java/com/example/gusteau/data/model/Meal.java
@@ -1,5 +1,6 @@
 package com.example.gusteau.data.model;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import com.google.gson.annotations.SerializedName;
@@ -9,6 +10,7 @@ public class Meal {
 
     @PrimaryKey
     @SerializedName("idMeal")
+    @NonNull
     private String id;
 
     @SerializedName("strMeal")
@@ -58,6 +60,167 @@ public class Meal {
     private String ingredient12;
     @SerializedName("strIngredient13")
     private String ingredient13;
+
+    public void setIngredient3(String ingredient3) {
+        this.ingredient3 = ingredient3;
+    }
+
+    public void setIngredient1(String ingredient1) {
+        this.ingredient1 = ingredient1;
+    }
+
+    public void setIngredient2(String ingredient2) {
+        this.ingredient2 = ingredient2;
+    }
+
+    public void setIngredient4(String ingredient4) {
+        this.ingredient4 = ingredient4;
+    }
+
+    public void setIngredient5(String ingredient5) {
+        this.ingredient5 = ingredient5;
+    }
+
+    public void setIngredient6(String ingredient6) {
+        this.ingredient6 = ingredient6;
+    }
+
+    public void setIngredient7(String ingredient7) {
+        this.ingredient7 = ingredient7;
+    }
+
+    public void setIngredient8(String ingredient8) {
+        this.ingredient8 = ingredient8;
+    }
+
+    public void setIngredient9(String ingredient9) {
+        this.ingredient9 = ingredient9;
+    }
+
+    public void setIngredient10(String ingredient10) {
+        this.ingredient10 = ingredient10;
+    }
+
+    public void setIngredient11(String ingredient11) {
+        this.ingredient11 = ingredient11;
+    }
+
+    public void setIngredient12(String ingredient12) {
+        this.ingredient12 = ingredient12;
+    }
+
+    public void setIngredient13(String ingredient13) {
+        this.ingredient13 = ingredient13;
+    }
+
+    public void setIngredient14(String ingredient14) {
+        this.ingredient14 = ingredient14;
+    }
+
+    public void setIngredient15(String ingredient15) {
+        this.ingredient15 = ingredient15;
+    }
+
+    public void setIngredient16(String ingredient16) {
+        this.ingredient16 = ingredient16;
+    }
+
+    public void setIngredient17(String ingredient17) {
+        this.ingredient17 = ingredient17;
+    }
+
+    public void setIngredient18(String ingredient18) {
+        this.ingredient18 = ingredient18;
+    }
+
+    public void setIngredient19(String ingredient19) {
+        this.ingredient19 = ingredient19;
+    }
+
+    public void setIngredient20(String ingredient20) {
+        this.ingredient20 = ingredient20;
+    }
+
+    public void setMeasure1(String measure1) {
+        this.measure1 = measure1;
+    }
+
+    public void setMeasure2(String measure2) {
+        this.measure2 = measure2;
+    }
+
+    public void setMeasure3(String measure3) {
+        this.measure3 = measure3;
+    }
+
+    public void setMeasure4(String measure4) {
+        this.measure4 = measure4;
+    }
+
+    public void setMeasure5(String measure5) {
+        this.measure5 = measure5;
+    }
+
+    public void setMeasure6(String measure6) {
+        this.measure6 = measure6;
+    }
+
+    public void setMeasure7(String measure7) {
+        this.measure7 = measure7;
+    }
+
+    public void setMeasure8(String measure8) {
+        this.measure8 = measure8;
+    }
+
+    public void setMeasure9(String measure9) {
+        this.measure9 = measure9;
+    }
+
+    public void setMeasure10(String measure10) {
+        this.measure10 = measure10;
+    }
+
+    public void setMeasure11(String measure11) {
+        this.measure11 = measure11;
+    }
+
+    public void setMeasure12(String measure12) {
+        this.measure12 = measure12;
+    }
+
+    public void setMeasure13(String measure13) {
+        this.measure13 = measure13;
+    }
+
+    public void setMeasure14(String measure14) {
+        this.measure14 = measure14;
+    }
+
+    public void setMeasure15(String measure15) {
+        this.measure15 = measure15;
+    }
+
+    public void setMeasure16(String measure16) {
+        this.measure16 = measure16;
+    }
+
+    public void setMeasure17(String measure17) {
+        this.measure17 = measure17;
+    }
+
+    public void setMeasure18(String measure18) {
+        this.measure18 = measure18;
+    }
+
+    public void setMeasure19(String measure19) {
+        this.measure19 = measure19;
+    }
+
+    public void setMeasure20(String measure20) {
+        this.measure20 = measure20;
+    }
+
     @SerializedName("strIngredient14")
     private String ingredient14;
     @SerializedName("strIngredient15")
